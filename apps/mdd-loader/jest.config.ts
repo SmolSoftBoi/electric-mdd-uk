1df5525ceaf16e5b1e5010913274751c1eb279be
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-06-13 15:00:52 +0000

    feat: add mdd-loader and mdd-loader-e2e applications with Jest and ESLint configurations
    - Created mdd-loader application with basic structure including main.ts and assets.
    - Added mdd-loader-e2e application for end-to-end testing.
    - Configured Jest for both applications with respective jest.config.ts files.
    - Set up ESLint configurations for both applications.
    - Implemented a simple CLI test in mdd-loader-e2e to verify output.
    - Updated package.json to include necessary dependencies for esbuild and axios.
    - Added TypeScript configurations for both applications.

---
+++ apps/mdd-loader/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'mdd-loader',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mdd-loader',
+};
