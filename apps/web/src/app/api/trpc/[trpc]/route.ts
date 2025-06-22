75395b6a1c059d2feb6cf610901a87fcd801d516
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-06-22 18:52:14 +0100

    feat(web): add tRPC route handler

--- apps/web/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,15 @@
+'use strict';
+
 import { handleTrpcRequest } from '../../../../server/trpc';
 
 export const runtime = 'nodejs';
 
+/**
+ * Handle GET requests for tRPC procedures.
+ */
 export const GET = handleTrpcRequest;
+
+/**
+ * Handle POST requests for tRPC procedures.
+ */
 export const POST = handleTrpcRequest;
